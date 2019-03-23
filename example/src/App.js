6dd65fc2c9ef25ddbcb4248abea058286d283634
SeongUk Han <hsw1824@gmail.com>
2019-03-23 16:35:46 +0900

    test

--- example/src/App.js
+++ example/src/App.js
@@ -3,12 +3,17 @@ import React, { Component } from "react";
 import DatawareCrop from "react-dataware-tool";
 
 export default class App extends Component {
+  
   render() {
     return (
-      <DatawareCrop 
-        width={440}
-        height={350}
-      />
+      <div>
+        <DatawareCrop 
+          // trakingInfo={}
+          width={440}
+          height={350}
+        />
+        <div>Preview</div>
+      </div>
     );
   }
 }
